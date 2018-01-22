45e2d3a22845b5e4324cd104fde25b226dbba613
Developer <developer@Developers-Mac.local>
2018-01-22 15:37:51 +0100

    La classe est finie

--- Accordeur/Tuning.swift
+++ Accordeur/Tuning.swift
@@ -8,15 +8,24 @@
 
 import Foundation
 
+extension String {
+    func dropLast(_ n: Int = 1) -> String {
+        return String(characters.dropLast(n))
+    }
+    var dropLast: String {
+        return dropLast()
+    }
+}
+
 class Tuning {
     
     init(pTuning: [String: Double]) {
-        Tuning = ["E2" : 82.41,
-                  "A2" : 110.00,
-                  "D3" : 146.83,
-                  "G3" : 196.00,
-                  "B3" : 246.94,
-                  "E4" : 329.63]
+        CurrentTuning = ["E2" : 82.41,
+                         "A2" : 110.00,
+                         "D3" : 146.83,
+                         "G3" : 196.00,
+                         "B3" : 246.94,
+                         "E4" : 329.63]
         wavePath = "C://"
     }
     
@@ -33,11 +42,21 @@ class Tuning {
                                  "A#" : 116.54,
                                  "B" : 123.47]
     
-    var Tuning: [String: Double]
+    var CurrentTuning: [String: Double]
     
     var wavePath: String
     
     static func getNoteFrequ(pNote: String, pOctave: Int) -> Double {
-        return frequencesList[pNote]! * Double((pOctave-2)*(pOctave-2))
+        return frequencesList[pNote]! * Double(truncating: pow(2, pOctave-2) as NSNumber)
+    }
+    
+    static func getNoteFrequV2(pNote: String) -> Double {
+        let pNotelol = " " + pNote
+        let pNotelol2 = pNote
+        let lolenculé = pNote.endIndex
+        let charactere = pNotelol[lolenculé]
+        let chaine = String(describing: charactere)
+        let octave = Double(chaine)
+        return frequencesList[pNotelol2.dropLast()]! * Double(truncating: pow(2, octave! - 2) as NSNumber)
     }
 }
