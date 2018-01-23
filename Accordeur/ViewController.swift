ad80b75a3e69f44a28e1e7f2467a2fee1b27003a
Moustanir Soulé-Ali <moustanir.souleali@gmail.com>
2018-01-23 07:39:39 +0100

    Try to render the view responsive

--- Accordeur/ViewController.swift
+++ Accordeur/ViewController.swift
@@ -10,19 +10,18 @@ import AudioKit
 import AudioKitUI
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-
+    
+    
     @IBOutlet private var noteChoice: UIPickerView!
     @IBOutlet private var accordChoice: UIPickerView!
-    @IBOutlet private var noteNameWithSharpsLabel: UILabel!
-    @IBOutlet private var noteNameWithFlatsLabel: UILabel!
     @IBOutlet private var audioInputPlot: EZAudioPlot!
 
     var mic: AKMicrophone!
     var tracker: AKFrequencyTracker!
     var silence: AKBooster!
-
+    var referenceFrequencyChosen = 40000
     let noteFrequencies = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87]
     let noteNamesWithSharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
     let noteNamesWithFlats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
@@ -38,28 +37,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         AKSettings.audioInputEnabled = true
         mic = AKMicrophone()
         tracker = AKFrequencyTracker(mic)
         silence = AKBooster(tracker, gain: 0)
+        //Préparer les pickersView
+        //Pour les notes
+        self.noteChoice.delegate = self
+        self.noteChoice.dataSource = self
+        //Pour les accords
+        self.accordChoice.delegate = self
+        self.accordChoice.dataSource = self
     }
-
+	
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-
         AudioKit.output = silence
         AudioKit.start()
-        //setupPlot()
         Timer.scheduledTimer(timeInterval: 0.1,
-                             target: self,
+                            target: self,
                              selector: #selector(ViewController.updateUI),
                              userInfo: nil,
                              repeats: true)
     }
     
+    /*Fonction comparant la fréquence reçue par le smartphone par rapport à la fréquence de référence*/
     func compareFrequencies(referenceFrequency:Float,frequencyEmitted:Float)->Float{
-        var difference :Float = (100 * frequencyEmitted)/referenceFrequency
+        let difference :Float = (100 * frequencyEmitted)/referenceFrequency
         var angle :Float = 0
         if difference > 100 {
             angle = -90
@@ -75,16 +79,49 @@ class ViewController: UIViewController {
         }
         return angle
     }
-    
+    /*Met à jour l'aiguille*/
     @objc func updateUI() {
-        rotate(compareFrequencies(referenceFrequency: 30000, frequencyEmitted: Float(tracker.frequency)))
+        print("Fréquence : \(tracker.frequency)")
+        rotate(compareFrequencies(referenceFrequency: Float(referenceFrequencyChosen), frequencyEmitted: Float(tracker.frequency)))
     }
+    
+    /*Début partie sélection note et accord*/
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    /*Renvoie le nombre de lignes*/
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var valueNumberRows = noteNamesWithSharps.count
+        if pickerView == noteChoice {
+            valueNumberRows = 6
+        }
+        return valueNumberRows
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        var data = noteNamesWithSharps[row]
+        if pickerView == noteChoice{
+            data = noteNamesWithFlats[row]
+        }
+        print("Data: \(data)")
+        return data
+    }
+    /*Renvoie la ligne choisie*/
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == noteChoice {
+            //TODO: Change la fréquence de référence pour la comparaison
+            //(cf avec Guillaume pour utiliser sa classe et les fonctions)
+        }
+        if pickerView == accordChoice {
+            //TODO:Faire la recherche et lié les boutons lançant les sons des cordes de la Guitare
+        }
+    }
+    /*Fin partie sélection note et accord */
     
     /*Début partie rotation aiguille*/
     @IBOutlet weak var Aiguille: UIImageView!
-    
     func rotate(_ degrees : Float){
-        
         UIView.animate(withDuration: 1.0, animations: {
             self.Aiguille.transform = CGAffineTransform(rotationAngle: CGFloat(degrees))
             })
